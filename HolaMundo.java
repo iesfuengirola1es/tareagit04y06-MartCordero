d13db12080a5252776c66eb3508fd1136d0b5a07
MartCordero <sistemas.info0605@gmail.com>
2025-04-27 23:27:00 +0200

    Añadida documentación Javadoc. Resolves #2

--- HolaMundo.java
+++ HolaMundo.java
@@ -1,4 +1,11 @@
+/**
+ * Clase que imprime un saludo en la consola.
+ */
 public class HolaMundo {
+    /**
+     * Método principal que ejecuta el programa.
+     * @param args Argumentos de la línea de comandos.
+     */
     public static void main(String[] args) {
         System.out.println("Hola, GitHub!");
     }
